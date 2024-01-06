ff5abb0203d5c6f85912509185629820c11bd501
roushan <rkc4096@gmail.com>
2024-01-06 16:10:56 +0530

    Connections.py updates

--- Connection.py
+++ Connection.py
@@ -1,6 +1,8 @@
 import socket
 import time
 import collections
+import struct
+from PLC_Comn.Data_maps import type_dict ,offset_map ,format_map
 
 class Packet:
     def __init__(self ,timestamp ,data):
@@ -19,21 +21,60 @@ class Connection(Packet):
         return Packet
     
     def establish_connection(self):
-        client=socket.socket(socket.AF_INET ,socket.SOCK_STREAM)
+        self.client=socket.socket(socket.AF_INET ,socket.SOCK_STREAM)
+        self.client.connect((self.host ,self.port))
         socket.setdefaulttimeout(self.timeout)
-        return client
     
-    def connection_handling():
+    def connection_handling(self):
+        print("log connection timeout ,timestamp :" ,time.time())
+        retry_interval=self.timeout
+        while True:
+            try:
+                self.client.connect((self.host ,self.port))
+                print("connection re-established")
+                break
+            except socket.error as e:
+                print("connection failed: ",e)
+                print("log required")
+                time.sleep(retry_interval)
+                retry_interval*=2
+        
+    def Alive_bit():
         pass
     
-    def on_timeout():
-        pass
-    
-    def receive(self ,conn):
-        client=conn.establish_connection()
-        data=client.recv(self.packet_size)
-        timestamp=time.time()
-        packet=Packet(timestamp ,data)
+    def decode(packet):
+        parsed_data={}
+        data=packet.data
+        for items in offset_map:
+            addr=items["address"]
+            name=items["name"]
+            type=items["type"]
+            offset_len=type_dict[type]
+            format_char=format_map[type]
+            temp_data=data[addr:addr+offset_len]
+            value=struct.unpack(">"+format_char ,temp_data)[0]
+            parsed_data[name]=value
+        return parsed_data
+ 
+    def receive(self):
+        self.establish_connection()
+        self.client.sendall(b'11')
+        while True:
+            try:
+                data=self.client.recv(self.packet_size)
+                if not data:
+                    print("log function implementation!")
+                else:
+                    timestamp=time.time()
+                    packet=Packet(timestamp ,data)
+                    data_map=self.decode(packet)
+                    notify(data_map)
+            except socket.timeout:
+                print("implement-log")
+                self.connection_handling(self)
+            time.sleep(self.refresh_rate)
+                    
+
         
         
         
