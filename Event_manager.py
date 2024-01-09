7f42ba8f78b7f8d6e3db1285c7033b8503e12d99
Roushan Kumar LM <roushan.k@lohiamk.com>
2024-01-09 18:17:25 +0530

    connections.py ,Dataclass.py ,event_manager.py

--- Event_manager.py
+++ Event_manager.py
@@ -1,15 +1,32 @@
 class Event_manager:
+    _instance=None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance=super(Event_manager ,cls).__new__(cls)
+        return cls._instance
+    
     def __init__(self):
-        self.subscribers=[]
+        self.event_callbacks={} #key=event ,value =list of callbacks
     
-    def subscribe(self ,subscriber):
-        self.subscribers.append(subscriber)
+    def subscribe(self ,event ,callback):
+        if event not in self.event_callbacks:
+            self.event_callbacks[event]=[]
+        self.event_callbacks[event].append(callback)
     
-    def unsubscribe(self ,subscriber):
-        self.subscribers.remove(subscriber)
+    def unsubscribe(self ,event ,callback):
+        if event in self.event_callbacks:
+            self.event_callbacks[event].remove(callback)
+            print(f"callback removed")
+        if not self.event_callabcks[event]:
+            del self.event_callbacks[event]
+            print(f"event : {event} deleted")
     
-    def notify(self ,event ,data_class):
-        for subscriber in self.subscribers:
-            subscriber.update(event ,data_class)
+    def publish(self ,event ,data):
+        if event in self.event_callbacks:
+            for callback in self.event_callbacks[event]:
+                callback(event ,data)
+        else :
+            print(f"cannot publish ,create event :{event} for the data ")
 
 
