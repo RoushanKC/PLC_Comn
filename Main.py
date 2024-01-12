7b086105b989bb76452c0b77f6800edbb0787f86
Roushan Kumar LM <roushan.k@lohiamk.com>
2024-01-12 18:01:26 +0530

    12 jan ,6.01pm

--- Main.py
+++ Main.py
@@ -3,15 +3,21 @@ import Dataclass
 import Event_manager
 import Data_maps
 import threading
+from Shared_data import Shared_data
 
-
+def console_data(q):
+    print(q)
+    
 if __name__=='__main__':
     host='192.168.0.52'
     port=2000
-    em=Event_manager.Event_manager()
-    conn=Connection.Connection(host ,port ,5740 ,2,1,em)
+    sh=Shared_data()
+    conn=Connection.Connection(host ,port ,5740 ,2,1,sh)
     thread1=threading.Thread(target=Connection.receive())
     thread1.start()
+    while True:
+        q=sh.update_queue.get()
+        console_data(q)
     
     
     
